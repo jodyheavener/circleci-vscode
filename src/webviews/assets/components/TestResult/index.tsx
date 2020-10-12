b43a73b133bb9d340a512236faaf04c0529760bb
Jody Heavener <j.heavener@gmail.com>
2020-10-12 19:04:50 -0400

    show test fail message

--- src/webviews/assets/components/TestResult/index.tsx
+++ src/webviews/assets/components/TestResult/index.tsx
@@ -7,10 +7,17 @@ import { l } from '../../lib/utils';
 import './index.scss';
 
 const TestResult = ({ test }: { test: JobTest }): JSX.Element => {
+  if (test.result === 'failure') {
+    console.log(test);
+  }
+
   return (
     <div className="test-result">
       <p className="test-classname">
-        <span>{test.classname}</span>
+        <span>
+          {test.source !== 'unknown' ? `${test.source} • ` : ''}
+          {test.classname}
+        </span>
 
         {test.result === 'success' && (
           <span className="test-duration">
@@ -26,6 +33,20 @@ const TestResult = ({ test }: { test: JobTest }): JSX.Element => {
 
         {test.name}
       </p>
+
+      {test.result === 'failure' && test.message && (
+        <details className="test-message">
+          <summary>{l('detailsTitle', 'See Details...')}</summary>
+          <div className="test-message-content">
+            {test.message.split('\n').map((text, index) => (
+              <React.Fragment key={`${text}-${index}`}>
+                {text}
+                <br />
+              </React.Fragment>
+            ))}
+          </div>
+        </details>
+      )}
     </div>
   );
 };
