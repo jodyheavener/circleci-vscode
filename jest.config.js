e40072557fb36c4fd6203064c5d3ca57ff6c73ca
Jody Heavener <j.heavener@gmail.com>
2022-02-05 11:51:46 -0400

    Add testing setup

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  clearMocks: true,
+  moduleFileExtensions: ['js', 'ts'],
+  testEnvironment: 'node',
+  testMatch: ['**/*.test.ts'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  verbose: true,
+  setupFiles: ['./src/setupTests.ts'],
+};
